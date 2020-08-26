5710fd0a246d3601b457c6ee04d0f7a530ed04d8
Shang Wang <wangshang19950911@gmail.com>
2020-08-25 22:34:31 -0400

    adding test rest api for back end

--- node-backend/app.js
+++ node-backend/app.js
@@ -2,11 +2,29 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
+// testing rest api 
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+ 
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+ 
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+ 
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
 app.set('port', port);
+
+// testing rest api with dynamic routing
+app.get('/:id', (req, res) => {
+  return res.send('Received a GET HTTP method ' + req.params.id);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
