d22acefdd9af870b6be2f3a076ec46691ef1804a
Shang Wang <wangshang19950911@gmail.com>
2020-09-06 17:07:47 -0400

    update the layouts

--- react-frontend/src/components/pages/user/body/summary.jsx
+++ react-frontend/src/components/pages/user/body/summary.jsx
@@ -1,11 +1,121 @@
 import React from "react";
+import {Tabs, Tab, Button, Row, Col, Form, Nav} from "react-bootstrap";
 
-export default function SummaryPage() {
+
+
+const LegendRow = (props)=>{
+  return (
+    <Row noGutters>
+      <Col>Event</Col>
+      <Col>Time</Col>
+      <Col>Note</Col>
+    </Row>
+  );
+}
+
+const DataRow = (props) => {
+  /* {Object.entries(props.data).map((x,i) => <Col key={i}>{x[1]}</Col>)} */
+  var data = props.data;
+  return (
+    <Row noGutters>
+      <Col>{data.event}</Col>
+      <Col>{data.time}</Col>
+      <Col>{data.note}</Col>
+    </Row>
+  );
+};
+
+const DataTabular = (props) => {
+  const rows = props.rows;
   return (
     <div>
-        <h1>This is SUMMERY Page!</h1>
+      <LegendRow />
+      {rows.map((x, i) => (
+        <DataRow key={i} data={x} />
+      ))}
     </div>
-    );
+  );
+};
+
+const SummaryCard = (props) =>  {
+  const data = props.data;
+  return (
+    <div>
+      <p>
+        <span>{data.category}</span> {data.time}
+      </p>
+      <DataTabular rows={data.events}/>
+      <br/>
+    </div>
+  );
+}
+
+const SummaryBody = (props) => {
+  const span = props.data.span;
+  const d = props.data.d;
+  console.log(d);
+  // Drop down box
+  return (
+    <div>
+      <p>Time Span: {span}</p>
+      {d.map((x, i) => (
+        <SummaryCard key={i} data={x} />
+      ))}
+    </div>
+  );
+};
+
+export default function SummaryPage() {
+  const res = {
+    day: {
+      span: "Sep.06, 00:00 - Sep.06 23:59",
+      d: [
+        {
+          category: "work",
+          time: "10 hours 51 min",
+          events: [
+            { event: "thesis", note: "i donw know", time: "2 hours 2 min" },
+            {
+              event: "build robot",
+              note: "i donw know",
+              time: "3 hours 2 min",
+            },
+          ],
+        },
+        {
+          category: "life",
+          time: "2 hours 51 min",
+          events: [
+            { event: "eating", note: "i donw know", time: "2 hours 2 min" },
+            { event: "sleeping", note: "i donw know", time: "3 hours 2 min" },
+          ],
+        },
+      ]
+    },
+    week: {
+      span: "Sep.06, 00:00 - Sep.12 23:59",
+      d: []
+    },
+    month: {
+      span: "Sep.01, 00:00 - Sep.30 23:59",
+      d: []
+    },
+  };
+  return (
+    <div>
+      <Tabs defaultActiveKey='day' id='uncontrolled-tab-example'>
+        <Tab eventKey='day' title='Today'>
+          <SummaryBody data={res.day} />
+        </Tab>
+        <Tab eventKey='week' title='This Week'>
+          <SummaryBody data={res.week} />
+        </Tab>
+        <Tab eventKey='month' title='This Month'>
+          <SummaryBody data={res.month} />
+        </Tab>
+      </Tabs>
+    </div>
+  );
 }
 
 
--- react-frontend/src/components/pages/user/body/today.jsx
+++ react-frontend/src/components/pages/user/body/today.jsx
@@ -14,20 +14,28 @@ const LegendRow = (props)=>{
   );
 }
 
-const DataRow = (props)=>{
+const DataRow = (props) => {
+  /* {Object.entries(props.data).map((x,i) => <Col key={i}>{x[1]}</Col>)} */
+  var data = props.data;
   return (
     <Row noGutters>
-      <Col>18:00 </Col>
-      <Col>Work</Col>
-      <Col>Writing thesis</Col>
-      <Col>literature survey of the jumping system in the field</Col>
-      <Col>completed 3 papers and wrote notes</Col>
-      <Col>Tag1 Tag2</Col>
+      <Col>{data.time}</Col>
+      <Col>{data.category}</Col>
+      <Col>{data.event}</Col>
+      <Col>{data.description}</Col>
+      <Col>{data.note}</Col>
+      <Col>
+        {data.tag.map((x, i) => (
+          <span key={i}>{x} </span>
+        ))}
+      </Col>
     </Row>
   );
 };
 
-const InputRow = (props) => {
+const InputGroup = (props) => {
+  const categories = props.categories;
+  const tags = props.tags;
   return (
     <Form>
       <Row noGutters>
@@ -38,9 +46,9 @@ const InputRow = (props) => {
         <Form.Group as={Col} controlId='time-tracking-item-category'>
           <Form.Label>Category</Form.Label>
           <Form.Control as='select'>
-            <option>category 1</option>
-            <option>category 2</option>
-            <option>category 3</option>
+            {categories.map((x, i) => (
+              <option key={i}>{x.category}</option>
+            ))}
           </Form.Control>
         </Form.Group>
 
@@ -52,43 +60,87 @@ const InputRow = (props) => {
             <option>event 3</option>
           </Form.Control>
         </Form.Group>
-        </Row>
+      </Row>
 
-        <Form.Group controlId='time-tracking-item-tag'>
-          <Form.Label>Tags</Form.Label>
-          <Form.Control type='text' placeholder='fancy dropdown box' />
-        </Form.Group>
-        {/* <Form.Group as={Col} controlId='time-tracking-item-project'>
-          <Form.Control type='text' placeholder='Dropdown box' />
-        </Form.Group> */}
-        <Form.Group controlId='time-tracking-item-description'>
-          <Form.Label>Description</Form.Label>
-          <Form.Control type='text' placeholder='descripton' />
-        </Form.Group>
-        <Form.Group controlId='time-tracking-item-note'>
-          <Form.Label>Note</Form.Label>
-          <Form.Control type='text' placeholder='Note' />
-        </Form.Group>
+      <Form.Group controlId='time-tracking-item-tag'>
+        <Form.Label>Tags</Form.Label>
+          <Form.Control as='select'>
+            {tags.map((x, i) => (
+              <option key={i}>{x}</option>
+            ))}
+          </Form.Control>
+      </Form.Group>
+      <Form.Group controlId='time-tracking-item-description'>
+        <Form.Label>Description</Form.Label>
+        <Form.Control type='text' placeholder='descripton' />
+      </Form.Group>
+      <Form.Group controlId='time-tracking-item-note'>
+        <Form.Label>Note</Form.Label>
+        <Form.Control type='text' placeholder='Note' />
+      </Form.Group>
+    <Button>Update</Button>
     </Form>
   );
 };
 
 export default function HistoryDetail() {
+  const categories = [
+    { category: "work", event: ['thesis', 'time tracker', 'job hunting'] },
+  ];
+
+  const tags = [
+    'headache',
+    'depressed',
+    'sick'
+  ]
+  const data = [
+    {
+      time: "18:00",
+      category: "work",
+      event: "thesis",
+      description: "5 pages words and equations",
+      note: "i donw know",
+      tag: ["headache", "depressed"],
+    },
+    {
+      time: "18:00",
+      category: "other",
+      event: "eating",
+      description: "dish washing",
+      note: "i donw know",
+      tag: ["headache", "depressed", "sad", "happy"],
+    },
+    {
+      time: "18:00",
+      category: "work",
+      event: "thesis",
+      description: "5 pages words and equations",
+      note: "i donw know",
+      tag: ["headache", "depressed"],
+    },
+    {
+      time: "18:00",
+      category: "work",
+      event: "thesis",
+      description: "5 pages words and equations",
+      note: "i donw know",
+      tag: ["headache", "depressed"],
+    },
+  ];
   return (
     <Row noGutters>
       <Col>
         <LegendRow />
         <br />
-        <DataRow />
-        <DataRow />
-        <DataRow />
+        {data.map((x, i) => (
+          <DataRow key={i} data={x}></DataRow>
+        ))}
         <br />
         {/* <LegendRow />
         <br /> */}
         <h3>Add a new record</h3>
-        <InputRow />
+        <InputGroup categories={categories} tags={tags}/>
 
-        <Button>Update</Button>
       </Col>
     </Row>
   );
